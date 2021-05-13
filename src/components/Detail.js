7c2ff1b4bb409e8f24324c00ed31c434474ceecb
Priscilla <priscillakung@gmail.com>
2021-05-12 23:40:32 -0700

    added login page with call to action

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -70,6 +70,7 @@ const ImageTitle = styled.div`
   min-height: 170px;
   width: 35vw;
   min-width: 200px;
+  margin-top: 60px;
 
   img {
     width: 100%;
@@ -142,4 +143,5 @@ const Description = styled.div`
   line-height: 1.4;
   font-size: 20px;
   margin-top: 16px;
+  max-width: 760px;
 `;
